defaea1ac1cca37328b19b9d14e44cd24bd3846c
wings11 <wykyaw2001@gmail.com>
2025-04-25 02:15:26 +0700

    s

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -13,14 +13,14 @@ const Home = () => {
     const menuPath = `/PopularPicks${table && token ? `?table=${table}&token=${token}` : ''}`;
 
     return (
-        <div className="bg" style={{ backgroundImage: `url(${assets.cover})` }}>
+        <div className="bg" style={{ backgroundImage: `url($'https://res.cloudinary.com/dakgb0zrs/image/upload/v1745521774/cover_xwlw5x.jpg')` }}>
             <div className="logo">
                 <img src='https://res.cloudinary.com/dakgb0zrs/image/upload/v1745480251/logo_mw6gtv.svg' alt="Logo" />
             </div>
             <div className="menu">
                 <Link to={menuPath}>
                     MENU
-                    <img src={assets.Arrow} alt="to menu" />
+                    <img src='https://res.cloudinary.com/dakgb0zrs/image/upload/v1745521814/Arrow_1_2_exxfg1.svg' alt="to menu" />
                 </Link>
             </div>
         </div>
